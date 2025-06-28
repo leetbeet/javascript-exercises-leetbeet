8919aea1edc6c6ed73efffa6a255e2de8d151974
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-06-28 11:45:58 +0800

    Finish exercise 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0 || !(Number.isInteger(num1)) || !(Number.isInteger(num2))) return "ERROR";
 
+    total = 0;
+
+    if (num1 > num2) [num1, num2] = [num2, num1];
+
+    for (let i = num1; i <= num2; i++) total += i;
+    return total;
 };
 
 // Do not edit below this line
