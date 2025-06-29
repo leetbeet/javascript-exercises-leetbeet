be98c14dd0568fcb691a86e6cd88618af2e764ea
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-06-29 12:56:16 +0800

    Finish exercise 12_calculators

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce((total, current) => {
+    return total + current;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+	return arr.reduce((total, current) => {
+    return total * current;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	if (n === 0) return 1;
+
+  let val = 1;
+  for (let i = 2; i <= n; i++) {
+    val *= i;
+  }
+  
+  return val;
 };
 
 // Do not edit below this line
