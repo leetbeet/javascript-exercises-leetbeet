2aad93075ead5629a7fab967645dd2777e9b9d53
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-06-29 13:28:23 +0800

    Finish exercise 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let maxAge = -1;
+    let oldestPerson = null;
 
+    for (const person of people) {
+        if (isNaN(person.yearOfDeath)) person.yearOfDeath = 2025;
+        const age = person.yearOfDeath - person.yearOfBirth;
+
+        if (age > maxAge) {
+            maxAge = age;
+            oldestPerson = person;
+        }
+    }
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
