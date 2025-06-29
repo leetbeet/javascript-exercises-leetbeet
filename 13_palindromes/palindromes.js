2fd44fbc7e827396fdc69b18d07aa382ffbbfc7a
leetbeet <136890775+leetbeet@users.noreply.github.com>
2025-06-29 13:08:46 +0800

    Finish exercise 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (text) {
+    text = text.toLowerCase();
 
+    textArr = text.split("");
+    for (let i = 0; i < textArr.length; i++) {
+        if (!(textArr[i] >= 'a' && textArr[i] <= 'z') && !(textArr[i] >= '0' && textArr[i] <= '9')) {
+            textArr.splice(i, 1);
+            i--;
+        }
+    }
+
+    for (let i = 0; i < Math.ceil(textArr.length/2); i++) {
+        if (textArr[i] !== textArr[textArr.length-i-1]) return false;
+    }
+
+    return true;
 };
 
 // Do not edit below this line
